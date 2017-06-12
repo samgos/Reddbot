c3ed8dd4b156101b2a7e8f6bd2c982a3a3b508e6
samgos <samuel.jj.gosling@gmail.com>
2017-06-12 02:05:47 +0100

    Create command_spec.rb

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe Command do
   let(:params) { 
     { 
-      'trigger_word' =>'dogetipper', 
+      'trigger_word' =>'reddbot', 
       'user_name' => 'Samson', 
       'user_id' => '12345' 
     }
   }
 
   it "Should parse the balance command" do 
-    params['text'] = 'dogetipper balance'
+    params['text'] = 'reddbot balance'
     c = Command.new params
     c.action.should eq "balance"
     c.user_name.should eq "Samson"
   end
 
   it "should know my balance is 0" do
-    params['text'] = 'dogetipper balance'
+    params['text'] = 'reddbot balance'
     c = Command.new params
     c.perform
     c.result.should eq "Balance for Samson is 0.0"
