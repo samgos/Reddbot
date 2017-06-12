202a0591fa084d16adfe849ebf3580320b8c4522
samgos <samuel.jj.gosling@gmail.com>
2017-06-12 02:57:56 +0100

    Create reddcoin.rb

--- coin_config/reddcoin.rb
+++ coin_config/reddcoin.rb
@@ -1,14 +1,14 @@
 module Reddcoin
   PERFORM_ERROR = 'Invalid crypto command.'
   BALANCE_REPLY_PRETEXT = 'Your current balance is: '
-  CURRENCY_ICON = 'RDD'
+  CURRENCY_ICON = ' RDD'
   WEALTHY_UPPER_BOUND = 10000
   WEALTHY_UPPER_BOUND_POSTTEXT = ' Are you a dev?!'
   WEALTHY_UPPER_BOUND_EMOJI = ':moneybag:'
-  BALANCE_REPLY_POSTTEXT = ' Rolling in redd.'
+  BALANCE_REPLY_POSTTEXT = '  Rolling in redd.'
   DEPOSIT_PRETEXT = 'Enter an integer for deposit'
   DEPOSIT_POSTTEXT = 'this is your depositing address'
-  TIP_ERROR_TEXT = 'pls say tip @username amount'
+  TIP_ERROR_TEXT = 'Please say tip @username amount'
   TIP_PRETEXT = 'Give a little to get a little, they do say?'
   TIP_POSTTEXT1 = 'https://live.reddcoin.com/'
   TIP_POSTTEXT2 = '| Reddcoin block explorer.'
@@ -17,7 +17,7 @@ module Reddcoin
   NETWORKINFO_ICON = ':bar_chart:'
   TOO_POOR_TEXT = 'Insafficent funds.'
   NO_PURPOSE_LOWER_BOUND_TEXT = 'You call that a tip?'
-  NO_PURPOSE_LOWER_BOUND = 0.00000001
+  NO_PURPOSE_LOWER_BOUND = 0.9
   RANDOMIZED_EMOJI = ':reddcoin:'
   NETWORK = 'reddcoin'
 end
