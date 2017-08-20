25a71c08505643921a6706a1d398b094274acbfc
samgos <samuel.jj.gosling@gmail.com>
2017-08-20 16:55:23 +0100

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -28,6 +28,8 @@ var dict_counts = [];
 var users = []
 var score = []
 var board = []
+var freq = []
+var tip_top = []
 for (var i = 0; i < 350 ; i++)                                          {
 var str = res.messages.matches[i].text;
 var stdout = capcon.captureStdout(function scope() {
@@ -41,6 +43,7 @@ var stdout = capcon.captureStdout(function scope() {
 for (var x = 76 ; x < 85 ; x++) {
 process.stdout.write(str[x]);
                                 }                       });
+var extract = JSON.stringify(stdout.match(/.{0,9}/g));
 var parse = extract.indexOf(/.RDD|RD|R|D|:| |r|e/g)
 if (parse = -1)
 {
@@ -57,15 +60,20 @@ function checkAndAdd(id,format) {
 if (!found) {
                 var x = format.substr(0, 9) + " " + format.substr(9);
                 dict_tips.push({key: id})
+                dict_counts.push({key: id})
                 var total = x.split(" ")
                 dict_tips[id] = "";
                 dict_tips[id] += total
                 users.push(id)
+                dict_counts[id] = "";
+                dict_counts[id] += 1 + ",";
                 }
         else
-                        var x = format.substr(0, 9) + " " + format.substr(9)
+        {
+                var x = format.substr(0, 9) + " " + format.substr(9)
                 var total = x.split(" ")
                 dict_tips[id] += total
+                dict_counts[id] += 1 + ",";
         }
 
 
@@ -75,7 +83,7 @@ if (!found) {
 
 
 }
-
+    
 for (var x = 0 ; x < dict_tips.length ; x++)
 {
 var key = users[x]
@@ -83,7 +91,7 @@ score.push(dict_tips[key])
 }
 
 const data = score
-                
+
 const sums = Object.keys(data).reduce((results, key) => {
   results[key] = (data[key] + '').split(',')
     .map(item => parseFloat(item))
@@ -97,10 +105,10 @@ const sums = Object.keys(data).reduce((results, key) => {
 for (var x = 0 ; x < dict_tips.length ; x++){
 board.push(parseFloat(sums[x]))                 }
 
-var leader = Math.max.apply(Math, board);
+    var leader = Math.max.apply(Math, board);
 var first = board.indexOf(parseFloat(leader));
 board.splice(first,1,0)
-board.pushfi
+
 var follower = Math.max.apply(Math, board);
 var second =  board.indexOf(parseFloat(follower))
 board.splice(second,1,0)
@@ -132,25 +140,62 @@ board.splice(oct,1,0)
 var nine = Math.max.apply(Math, board);
 var nes  =  board.indexOf(parseFloat(nine));
 
-console.log(sums)
-console.log(dict_tips)
 
-web.chat.postMessage(message.channel,'Tip to win a name amongst the elite.',
-   {
-     username: 'reddbot',
-     icon_emoji: ':reddcoin:',
-     attachments: JSON.stringify([{
-                                   colour: 'good',
+for (var x = 0 ; x < dict_counts.length ; x++)
+{
+var key = users[x]
+freq.push(dict_counts[key])
+}
+
+const dataz = freq
+
+const tipper = Object.keys(dataz).reduce((results, key) => {
+  results[key] = (dataz[key] + '').split(',')
+    .map(item => parseInt(item))
+    .filter(item => !isNaN(item))
+    .reduce((res, item) => res + item, 0)
+
+  return results
+}, {})
+
+for (var x = 0 ; x < dict_counts.length ; x++){
+tip_top.push(parseInt(tipper[x]))                 }
+
+var recent = Math.max.apply(Math, tip_top);
+var location = tip_top.indexOf(parseInt(recent));
+tip_top.splice(location,1,0)
+
+var recent2 = Math.max.apply(Math, tip_top);
+var location2 = tip_top.indexOf(parseInt(recent2));
+tip_top.splice(location2,1,0)
+
+var recent3 = Math.max.apply(Math, tip_top);
+var location3 = tip_top.indexOf(parseInt(recent3));
+
+var attach =                    [{
                                    title: 'Leaderboard',
                                    fields: [{
-                                             value: "1. <@" + users[first].slice(0,11) + ">  Tipped: " + leader + " RDD :reddcoin:"},
+                                              value: "1. <@" + users[first].slice(0,11) + ">  Tipped: " + leader + " RDD :reddcoin:"},
                                              {value: "2. <@" + users[second].slice(0,11) + ">  Tipped: " + follower + " RDD :reddcoin:"},
                                              {value: "3. <@" + users[third].slice(0,11) + ">  Tipped: " + bronze + " RDD :reddcoin:"},
                                              {value: "4. <@" + users[fourth].slice(0,11) + ">  Tipped: " + four + " RDD :reddcoin:"},
                                              {value: "5. <@" + users[fifth].slice(0,11) + ">  Tipped: " + five + " RDD :reddcoin:"},
                                              {title: "Upcoming Tippers",
-                                             value:  "<@" + users[sixth].slice(0,11) + "> ,<@" + users[seventh].slice(0,11) + ">,<@" + users[oct].slice(0,11) + ">"}
-                                             ]}])
+                                             value:  "<@" + users[sixth].slice(0,11) + ">,<@" + users[seventh].slice(0,11) + ">,<@" + users[oct].slice(0,11) + ">",
+                                             short: 'true'},
+                                             {title: "Most Frequent Tippers",
+                                             value:  "<@" + users[location].slice(0,11) + ">,<@" + users[location2].slice(0,11) + ">,<@" + users[location3].slice(0,11$
+                                             short: 'true'}
+                                        ],
+                                   color: '#2BB24B'}]
+
+var table =  JSON.stringify(attach)
+                                              
+web.chat.postMessage(message.channel,'Tip to win a name amongst the elite.',
+   {
+     username: 'reddbot',
+     icon_emoji: ':reddcoin:',
+     attachments: table,
 
          },
 function(err, res) {
@@ -168,4 +213,5 @@ function(err, res) {
 
 }
 });
+
 
