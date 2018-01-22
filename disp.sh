0c6c4d33cf203436ab17187b10fb468ea3171c5b
Samuel JJ Gosling <samuel.jj.gosling@gmail.com>
2018-01-22 14:04:30 +0000

    Delete disp.sh

--- disp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-a=$(lynx -base -source 'https://coinmarketcap.com/currencies/reddcoin/' | hxnormalize -x | \
-    hxselect -c -s '\n' "body > div.container > div > div.col-xs-12.col-sm-12.col-md-12.col-lg-10 > div:nth-child(5) > div.col-xs-6.col-sm-8.col-md-4.text-left > small.text-gray")
-str=$(echo $a)
-printf $str 
-
-
